16cc5e60fff583e8554101a7c38acdec058fc222
Karan Nair <elitekarry@gmail.com>
2026-02-24 10:18:09 +0530

    Add files via upload

---
+++ calci.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    int a;
+    int b;
+    printf("Enter Your first no: ");
+    scanf("%d", &a);
+    printf("Enter your second no: ");
+    scanf("%d", &b);
+    printf("Addition: %d\n",a+b);
+    printf("Subtraction: %d\n",a-b);
+    printf("Multiplication: %d\n",a*b);
+    printf("Divsion: %d\n",a/b);
+    return 0;
+
+}
